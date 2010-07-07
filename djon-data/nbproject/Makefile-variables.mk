954c2d1dfab3e6aa77af8fc774faed99adaac1a7
cross <cross@dev-juanc>
2010-07-07 11:11:22 -0500

    New Version of Djon (start from scratch)

---
+++ djon-data/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=libdjon-data.so
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/libdjon-data.so
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=libdjon-data.so.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libdjon-data.so.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=libdjon-data.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libdjon-data.so
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=libdjon-data.so.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libdjon-data.so.tar
