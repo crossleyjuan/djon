341fac8bcf1d5e4a1843a4fdd000ddd71647acf3
Cross <crossleyjuan@gmail.com>
2010-10-12 22:05:26 -0500

    estructura de directorios en debian

--- installer/debian.sh
+++ installer/debian.sh
@@ -1,4 +1,24 @@
+find . -type d | xargs chmod 755
 fakeroot dpkg-deb --build debian
 lintian debian.deb
-dpkg-deb --build debian
-find . -type d | xargs chmod 755
+
+Estructura de directorios:
+debian:
+DEBIAN
+usr
+
+debian/DEBIAN:
+control
+
+debian/usr:
+bin
+lib
+
+debian/usr/bin:
+djon
+
+debian/usr/lib:
+libdjon-util.so
+libdjon-util.so.1
+libdjon-util.so.1.0
+libdjon-util.so.1.0.0
